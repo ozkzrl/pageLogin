f2938da83c82b0352713ab98c1b57b0ccdb7864e
ozkzrl <ozkan.zorlu@gmail.com>
2023-12-01 00:20:38 +0100

    last registration completed

---
+++ pageLogin/default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace pageLogin
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn=new SqlConnection(@"Data Source=DESKTOP-UQPST9J;Initial Catalog=LoginDB;Integrated Security=True"))
+            {
+                conn.Open();
+                string query = "select count(1) from tblUser where UserName=@UserName and Password=@Password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@UserName",txtUserName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Password",txtPassword.Text.Trim());
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                if(count==1)
+                {
+
+                    Session["UserName"] = txtUserName.Text.Trim();
+                    Response.Redirect("ogtmen.aspx");
+                }
+
+
+
+
+            }
+        }
+    }
+}
---
+++ pageLogin/ogtmen.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pageLogin
+{
+    public partial class ogtmen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
